2740bf0f0197a1b4c4f389e7ef43bacec8276e7e
Rushikesh Gunthe <guntherushi111@gmail.com>
2025-10-16 15:01:50 +0530

    Day_03 in class
    In this folder all the programs are practiced in class of logic building for practice

---
+++ classwork/Day_03/program32.c
@@ -0,0 +1,21 @@
+// 1    *   2    *  3   *   4   *   5   *
+
+#include<stdio.h>
+
+void Display()
+{
+    printf("1\t*\t",iCnt);
+    printf("2\t*\t",iCnt);
+    printf("3\t*\t",iCnt);
+    printf("4\t*\t",iCnt);
+    printf("5\t*\t",iCnt);
+
+    printf("\n");
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
---
+++ classwork/Day_03/program50.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void DisplayNonFactors(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <= (iNo/2); iCnt++)    
+    {
+        if((iNo % iCnt) == 0)
+        {
+            printf("%d\n",iCnt);
+        }
+    }
+}
+//Time Complexity: O(N/2)
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter a number:\n");
+    scanf("%d",&iValue);
+    
+    DisplayNonFactors(iValue);
+    
+    return 0;
+}
